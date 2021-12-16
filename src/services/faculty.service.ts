17df5a7723ded80843720ed34dc90a3eb4fa6f7f
Vic Flores <vicsito2014@gmail.com>
2021-12-16 10:15:31 -0600

    feat: faculty service created and working

---
+++ src/services/faculty.service.ts
@@ -0,0 +1,54 @@
+import { QueryResult } from 'pg';
+import { pool } from '../database';
+import { Ifaculty } from '../interfaces/Ifaculty';
+
+export class Faculty {
+  async findFaculties() {
+    const res: QueryResult = await pool.query('SELECT * FROM faculties');
+    return res.rows;
+  }
+
+  async findFacultyById(id: string) {
+    const res: QueryResult = await pool.query(
+      'SELECT * FROM faculties WHERE id = $1',
+      [id]
+    );
+
+    return res.rows;
+  }
+
+  async findFacultyByNameAndSchool(name: string, school: string) {
+    const res: QueryResult = await pool.query(
+      'SELECT * FROM faculties WHERE name = $1 AND school = $2',
+      [name, school]
+    );
+
+    return res.rows;
+  }
+
+  async createFaculty(body: Ifaculty) {
+    await pool.query('INSERT INTO faculties (name, school) VALUES ($1, $2)', [
+      body.name,
+      body.school,
+    ]);
+
+    return 'Faculty created successfully';
+  }
+
+  async updateFaculty(id: string, body: Ifaculty) {
+    await this.findFacultyById(id);
+    await pool.query(
+      'UPDATE faculties SET name = $1, school = $2 WHERE id = $3',
+      [body.name, body.school, id]
+    );
+
+    return 'Faculty modified successfully';
+  }
+
+  async deleteFaculty(id: string) {
+    await this.findFacultyById(id);
+    await pool.query('DELETE FROM faculties WHERE id = $1', [id]);
+
+    return 'Faculty deleted successfully';
+  }
+}
