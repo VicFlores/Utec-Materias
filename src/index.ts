1131a1a5ee5f943f08081ffe05b1917990f97566
Vic Flores <vicsito2014@gmail.com>
2021-12-31 17:03:15 -0600

    feat: cors origin was added

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,11 @@ const app: express.Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'http://localhost:4000'],
+  })
+);
 app.use(helmet());
 
 routerApi(app);
